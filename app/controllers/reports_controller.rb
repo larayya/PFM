3ed4a51598566e889850fd8c95a85532bcfb22ef
Larayya <larayya@ukr.net>
2023-07-16 17:31:40 +0300

    'new2'

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,29 +1,4 @@
-# chat
-# class ReportsController < ApplicationController
-#   def index
-
-#   end
-  
-#   def report_by_category
-#     category = params[:category]
-#     @operations = Operation.where(category: category)
-#   end
-
-#   def report_by_dates
-#     start_date = params[:start_date]
-#     end_date = params[:end_date]
-#     @operations = Operation.where(date: start_date..end_date)
-#   end
-
-  
-# end
-
-# class ReportsController < ApplicationController
-#   def index
-#     # show categories
-#     @categories = Category.all 
-#   end
-  class ReportsController < ApplicationController
+class ReportsController < ApplicationController
     def index
       @categories = Category.all
       @operations_count = Operation.count
@@ -45,31 +20,15 @@
     end
   end
 
-  def report_by_category
-    @data = params
+    def report_by_category
+      @data = params
+      @categories = Category.all
+    end
+     
+    def report_by_dates
+      @data = params
+      @operations = Operation.order(:date)
+    end
+            
   end
-
-  def report_by_date
-    @data = params
-  end
-end
-
-# class ReportsController < ApplicationController
-#   def index
-#   end
-
-#   def report
-#     if params[:btn_date]
-#       redirect_to({
-#         action: "report_by_date",
-#         start_date: params[:start_date],
-#         end_date: params[:end_date]
-#       })
-#     elsif params[:btn_category] # Add this condition
-#       redirect_to({
-#         action: "report_by_category",
-#         category: params[:category]
-#       })
-#     end
-#   end
-# end
+  
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,43 +2,8 @@ class Category < ApplicationRecord
     has_many :operations
     validates :name, presence: true, uniqueness: true
     validates :description, presence: true
+  def calculate_amount
+      operations.sum(:amount)
+  end
 end
-  
-  # class Category < ApplicationRecord
-  #   def self.report_by_category(start_date, end_date)
-  #     # Retrieve the expenses for the specified date range
-  #     expenses = Operation.where(date: start_date..end_date, category_id: category_id)
-  
-  #     # Group the expenses by category
-  #     expenses_by_category = expenses.group_by(&:category)
-  
-  #     # Calculate the total amount for each category
-  #     report_data = expenses_by_category.map do |category, expenses|
-  #       total_amount = expenses.sum(&:amount)
-  #       { category: category.name, total_amount: total_amount }
-  #     end
-  
-  #     report_data
-  #   end
-  
-    # ... other methods ...
-  # end
-  
-  # class Category < ApplicationRecord
-  #   def self.report_by_category(start_date, end_date)
-  #     expenses_by_category = Operation
-  #       .joins(:category)
-  #       .where(date: start_date..end_date)
-  #       .group('categories.name')
-  #       .sum(:amount)
-  
-  #     report_data = expenses_by_category.map do |category_name, total_amount|
-  #       { category: category_name, total_amount: total_amount }
-  #     end
-  
-  #     report_data
-  #   end
-  
-  #   # ... other methods ...
-  # end
-  
+
